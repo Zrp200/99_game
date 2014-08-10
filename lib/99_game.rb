4bfec2c9bdabd845f02dfb1ab468123abd8a7856
Zachary Perlmutter <zrp200@gmail.com>
2014-08-10 15:15:23 -0700

    Streamlined code Fixed bugs in Object#converter
    Signed-off-by: Zachary Perlmutter <zrp200@gmail.com>

--- lib/99_game.rb
+++ lib/99_game.rb
@@ -16,17 +16,19 @@
 			return true
 		end
 	end
-# Converts input to an integer if String#capitalize does something. If _input_ is an abbreviation, _input_ is converted to what it stands for. Otherwise, it simply returns a capitalized version of _input_. If _input_ is nil or an emtpy string, raises a CardError
+# Converts input to an integer if String#capitalize does something. If parameter input is an abbreviation, _input_ is converted to what it stands for. Otherwise, it simply returns a capitalized version of _input_. If _input_ is nil or an emtpy string, raises a CardError
 	def converter(input)
 		abbrev = {"$" => "Joker", "K" => "King", "J" => "Jack", "Q" => "Queen", "A" => "Ace"}
-		if input == input.capitalize
-			return input.to_i
-		elsif not_nil? abbrev[ input ]
-			return abbrev[ input ]
-		elsif input.nil? || input == String.new
-			raise CardError, "Input not allowed"
+		if input.to_i == 0
+			case input.capitialize
+				when "$" then "Joker"
+				when "K" then "King"
+				when "J" then "Jack"
+				when "Q" then "Queen"
+				when "A" then "Ace"
+			end
 		else
-			return input
+			input.to_i
 		end
 	end
 # Expected errors
