fe54c17b0f1785e0ccfd2827f8e8b593032e2841
Zachary Perlmutter <zrp200@gmail.com>
2014-10-19 13:16:30 -0700

    Update hand_spec.rb

--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -1,4 +1,14 @@
 describe "CPU" do
-  subject{ Hand.new Deck.new}
-  subject.hand.hand = [Card.new("King", Card::SPADES), Card.new("Joker")]
+  subject {Hand.new Deck.new}
+  subject.hand.hand = [Card.new("King", Card::SPADES), Card.new("Joker"), Card.new(2, Card::DIAMONDS)]
+  describe "test" do
+    tests = proc {|index| test(subject.hand.hand[index], 50, nil)}
+    v1 = tests.call 0
+    v2 = tests.call 1
+    v3 = tests.call 2
+    describe v1 do
+      it {is_expected.to be > v2}
+      it {is_expected.to be > v3}
+    end
+  end
 end
