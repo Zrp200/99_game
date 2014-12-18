c5639a567c49f88eb73877cf1482bd5d90eea7c2
Zachary Perlmutter <zrp200@gmail.com>
2014-12-17 23:39:11 -0800

    Delete deck_spec.rb

--- lib/deck_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper.rb"
-require "99_game"
-create_deck = proc {Deck.new jokers: true}
-deck = create_deck.call
-describe Deck do
-	describe create_deck.call.cards.length do
-		it {is_expected.to eq 54}
-	end
-end
