811de820d210c6e0054eb3d4f4a180c4aea06a3a
Zachary Perlmutter <zrp200@gmail.com>
2014-12-17 23:08:09 -0800

    Create hand.rb

---
+++ lib/hand.rb
@@ -0,0 +1,32 @@
+class Hand # Creates an object that holds and can play cards. Interacts with Deck objects.
+	# The actual hand
+    	attr_accessor :hand
+	# Creates a new Hand. The deck argument tells the object which deck to use in Hand#play
+    	def initialize(deck_in_use)
+			@hand, @deck = [deck_in_use.draw, deck_in_use.draw, deck_in_use.draw], deck_in_use
+		end
+	# Gameplay method. The parameter 'card' is the card being played.
+    	def play(card)
+			if card.num == "King"; $value = 99
+			elsif card.num == "Joker"; $value = 0
+			else; $value += card.value
+			end
+			i, done = 0, false
+			for index in @hand
+				if index.num == card.num and not done
+					discard = @hand[ i ]
+					@hand.delete_at i
+					@hand.push @deck.draw
+					@deck.discard discard
+					done = true
+				end
+				i += 1
+			end
+		end
+	# Allows you to see your cards.
+    	def view
+		print "\tThese are your cards: "
+        	@hand.each {|card| print "\t#{card.num}"}
+    	end
+    	alias inspect hand
+end
