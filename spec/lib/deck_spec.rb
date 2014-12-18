df17f2539dbbd8eff23954a49d6f39e70c5b54f8
Zachary Perlmutter <zrp200@gmail.com>
2014-12-17 23:39:51 -0800

    Create deck_spec.rb

---
+++ spec/lib/deck_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper.rb"
+require "99_game"
+create_deck = proc {Deck.new jokers: true}
+deck = create_deck.call
+describe Deck do
+ describe create_deck.call.cards.length do
+		it {is_expected.to eq 54}
+	end
+end
