46e4694b8d0d945ee8d74984f018adb5eb82c7f2
Zachary Perlmutter <zrp200@gmail.com>
2014-12-17 23:06:36 -0800

    Create card.rb

---
+++ lib/card.rb
@@ -0,0 +1,17 @@
+class CardDeck::Card # Represents a card in the deck
+	# Value of the card
+		def value
+			return case @num
+				when "Ace" then 1
+				when 2..3 then @num
+				when 4 then 0
+				when 5..8 then @num
+				when 9 then 0
+				when 10 then 10
+				when "Jack" then 0
+				when "Queen" then -10
+				when "King" then 99
+				when "Joker" then 0
+			end
+		end
+end
