546a90788021187bdb7a57056f5d8b4ceaad0ab0
Zachary Perlmutter <zrp200@gmail.com>
2014-12-22 13:37:05 -0800

    Update hand_spec.rb

--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 require "hand"
 describe "CPU" do
-  s = Hand.new Deck.new
+  s = Hand.new
   s.hand = [Card.new("King", Card::SPADES), Card.new("Joker"), Card.new(2, Card::DIAMONDS)]
   describe "test" do
     tests = proc {|index| test(s.hand[index], 50, nil)}
@@ -15,19 +15,11 @@ describe "CPU" do
   end
 end
 describe Hand do
-	hand = Hand.new create_deck.call
+	hand = Hand.new
 	describe "#hand" do
 		describe "#length" do
 			subject { hand.hand.length }
 			it {is_expected.to eq 3}
-		end
-	end
-	describe "#new" do
-		describe "Deck#length" do
-			it "should have three less cards after initialization" do
-				deck1, hand, deck2 = deck.cards.length, Hand.new( deck ), deck.cards.length
-				expect( deck1 ).to be > deck2
-			end
 		end
 	end
 end
