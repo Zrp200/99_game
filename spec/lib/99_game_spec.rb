279833a05243b99fa10deb3a2c5441124cb9c0c5
Zachary Perlmutter <zrp200@gmail.com>
2014-12-22 13:33:14 -0800

    Update 99_game_spec.rb

--- spec/lib/99_game_spec.rb
+++ spec/lib/99_game_spec.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 require "99_game"
 describe "converter" do
 	context "when a number" do
-		subject {converter ?5}
-		it {is_expected.to eq 5}
+		subject { converter ?5 }
+		it { is_expected.to eq 5 }
 	end
 	context "when an abbreveation" do
-		subject {converter ?K}
+		subject { converter ?K }
 		it {is_expected.to eq "King"}
 	end
 end
