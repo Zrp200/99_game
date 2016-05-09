51e0251b7f2d09cf40e6318ef53538f0b680af41
Zachary Perlmutter <zrp200@gmail.com>
2016-05-09 10:15:34 -0700

    Update 99_game.rb

--- lib/99_game.rb
+++ lib/99_game.rb
@@ -22,11 +22,7 @@ autoload :Hand, "hand"
     	end
 # Tests if obj is not nil.
 	def not_nil?(obj)
-		if obj.nil?
-			false
-		else
-			true
-		end
+		!obj.nil?
 	end
 # Converts input to an integer if String#capitalize does something. If parameter input is an abbreviation, _input_ is converted to what it stands for. Otherwise, it simply returns a capitalized version of _input_. If _input_ is nil or an emtpy string, raises a CardError
 # Expected errors
@@ -50,7 +46,7 @@ Otherwise, it simply returns a capitalized version of input.
 def converter(input)
 	abbrev = {"$" => "Joker", "K" => "King", "J" => "Jack", "Q" => "Queen", "A" => "Ace"}
 	raise(CardError, "Input cannot be blank") if input == String.new
-	if input.to_i == 0
+	if input.to_i.zero?
 		case input.capitalize
 			when ?$ then "Joker"
 			when ?K then "King"
