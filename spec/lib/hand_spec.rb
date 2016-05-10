38e6ef50d154f703bd0220cf8427e12e33e95e5c
Zachary Perlmutter <zrp200@gmail.com>
2016-05-10 08:47:41 -0700

    Update hand_spec.rb

--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -4,7 +4,7 @@ describe "CPU" do
   s = Hand.new
   s.cards = [Card.new("King", Card::Spades), Card.new("Joker"), Card.new(2, Card::Diamonds)]
   describe "test" do
-    tests = proc {|index| test(s.cards[index], 50, nil)}
+    tests = proc {|index| card_test(s.cards[index], 50)}
     v1 = tests.call 0
     v2 = tests.call 1
     v3 = tests.call 2
